67c386c09fc6196e08042f868596e9c4604c5073
Edin Tufek <edintufek11@gmail.com>
2022-01-06 21:38:19 +0100

    dhdglkddg

--- Program.cs
+++ Program.cs
@@ -11,10 +11,11 @@ var connectionString = builder.Configuration.GetConnectionString("AzureContext")
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 //builder.Services.AddDbContext<DataContext>(options =>
-       // options.UseSqlServer(builder.Configuration.GetConnectionString("DataContext")));
+// options.UseSqlServer(builder.Configuration.GetConnectionString("DataContext")));
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<DataContext>();builder.Services.AddDbContext<DataContext>(options =>
-    options.UseSqlServer(connectionString));
+    .AddEntityFrameworkStores<DataContext>(); builder.Services.AddDbContext<DataContext>(options =>
+     options.UseSqlServer(connectionString));
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 CreateDbIfNotExists(app);
@@ -29,36 +30,41 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+});
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
 
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-    app.MapRazorPages();
+app.MapRazorPages();
 
 app.Run();
 
 
 static void CreateDbIfNotExists(IHost host)
+{
+    using (var scope = host.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        try
         {
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<DataContext>();
-                    //context.Database.EnsureCreated();
-                    DbInitializer.Initialize(context);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred creating the DB.");
-                }
-            }
+            var context = services.GetRequiredService<DataContext>();
+            //context.Database.EnsureCreated();
+            DbInitializer.Initialize(context);
         }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred creating the DB.");
+        }
+    }
+}
 
